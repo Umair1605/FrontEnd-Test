d9cc2f826d290bc9ac685ebce9c68af90445f7d4
Umair1605 <umair1605b@gmail.com>
2022-09-18 23:01:00 +0500

    Test Done

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import { ethers } from "ethers"
+import NFTAbi from './contractData/NFT.json'
+import NFTAddress from './contractData/NFT-address.json'
+import { Row, Col, Card, Button,Table } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import axios from 'axios'
+import { getSuggestedQuery } from '@testing-library/react';
+
+function App() {
+   
+  const [data,setData] = useState([]);
+  const getUser = async() => {
+    try {
+      const response = await axios.get("/api/getUser");
+      console.log(response.data);
+      setData(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(() => {
+    getUser();
+  },[])
+  // console.log("Data ... ",data);
+  return (
+    <div className="App">
+      <>
+      <Navbar expand="lg" bg="secondary" variant="dark">
+          <Container>
+              <Navbar.Brand>
+                  &nbsp;PLUTON
+              </Navbar.Brand>            
+          </Container>
+      </Navbar>
+      </>
+      <div >
+          <div className="flex justify-center">
+          <div className="px-5 container" style={{paddingTop:30 }}>
+            <Table  striped bordered hover variant="dark" >
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Account No.</th>
+                  <th>Balance</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((info,i)=>(
+                  // {console.log("info..",info)}
+                  // <p>{info}</p>
+                  <tr key={i}>
+                    <td>{i}</td>
+                    <td>{info.accountNo}</td>
+                    <td>{info.accountBalanace}</td>
+                  </tr>
+                ))}
+{/*                 
+                <tr>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Jacob</td>
+                  <td >Larry the Bird</td>
+                </tr> */}
+              </tbody>
+            </Table>
+            </div>
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
